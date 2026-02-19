a97edea48d27d20943a8e2075d3752919e726a77
Dhaneswar Hansdah <166326995+CodeFreakK-22@users.noreply.github.com>
2026-02-19 20:25:13 +0530

    fix: correct import path for Home component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
+import Home from './pages/home'
 import Navbar from './components/Navbar'
 import Collection from './pages/Collection'
 import About from './pages/About'
